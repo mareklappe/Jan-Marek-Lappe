f64f5f574557ccbfd8ccd5f9f11acd1d562e9814
Jan-Marek Lappe <janmarek@uni-bremen.de>
2017-12-10 22:01:47 +0100

    olmost final

---
+++ videoHover.js
@@ -0,0 +1,43 @@
+var videoList = document.querySelectorAll('video');
+
+$('#video1').hover(
+  function() {
+    $('#video1').get(0).play();
+}, function() {
+    $('#video1').get(0).pause();
+});
+
+$('#video2').hover(
+  function() {
+    $('#video2').get(0).play();
+}, function() {
+    $('#video2').get(0).pause();
+});
+
+$('#video3').hover(
+  function() {
+    $('#video3').get(0).play();
+}, function() {
+    $('#video3').get(0).pause();
+});
+
+$('#video4').hover(
+  function() {
+    $('#video4').get(0).play();
+}, function() {
+    $('#video4').get(0).pause();
+});
+
+$('#video5').hover(
+  function() {
+    $('#video5').get(0).play();
+}, function() {
+    $('#video5').get(0).pause();
+});
+
+$('#video6').hover(
+  function() {
+    $('#video6').get(0).play();
+}, function() {
+    $('#video6').get(0).pause();
+});
